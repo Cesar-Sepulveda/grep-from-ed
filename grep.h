4cb691362d46877c190168b217ecba5f7dbf1943
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-04-10 17:36:02 -0700

    Update grep.h

--- grep.h
+++ grep.h
@@ -1,85 +1,21 @@
-#define	BLKSIZE	4096
-#define	NBLK	2047
-#define	NULL	0
-#define	LBSIZE	4096
-#define FNSIZE 128
-#define	ESIZE	256
-#define	GBSIZE	256
-#define	NBRA	5
-#define	EOF	-1
-#define	CBRA	1
-#define	CCHR	2
-#define	CDOT	4
-#define	CCL	6
-#define	NCCL	8
-#define	CDOL	10
-#define	CEOF	11
-#define	CKET	12
-#define	CBACK	14
-#define	CCIRC	15
-#define	STAR	01
-#define	READ	0
-#define	WRITE	1
-#define	SIGHUP	1
-#define ARGC_ERROR 0
-#define BUFSIZE 100
-
+const int BLKSIZE=4096, NBLK=2047, NULL=0, LBSIZE=4096, FNSIZE=128;
+const int ESIZE=256, GBSIZE=256, NBRA=5, EOF=-1, CBRA=1, CCHR=2;
+const int CDOT=4, CCL=6, NCCL=8, CDOL=10, CEOF=11, CKET=12, CBACK=14;
+const int CCIRC=15, STAR=01, READ=0, WRITE=1, SIGHUP=1, ARGC_ERROR=0, BUFSIZE=100;
 typedef void	(*SIG_TYP)(int);
-
-char	Q[]	= "";
-char	T[]	= "TMP";
-int	peekc;
-int	lastc;
-char	savedfile[FNSIZE];
-char	file[FNSIZE];
-char	linebuf[LBSIZE];
-char	expbuf[ESIZE+4];
-int	given;
-unsigned int	*addr1, *addr2;
-unsigned int	*dot, *dol, *zero;
-char	genbuf[LBSIZE];
+char	Q[]	= "", T[]	= "TMP", ibuff[BLKSIZE], genbuf[LBSIZE];
+char obuff[BLKSIZE], inputbuf[GBSIZE], buf[BUFSIZE], line[70];
+int	peekc, lastc, given, ninbuf, io, pflag, oflag, listf, listn, col;
+int tline, ichanged, nleft, anymarks, nbra, subnewa, fchange, wrapp, bufp = 0;
+char	savedfile[FNSIZE], file[FNSIZE], linebuf[LBSIZE], expbuf[ESIZE+4];
+unsigned int	*addr1, *addr2, *dot, *dol, *zero;
 long	count;
-char	*nextip;
-char	*linebp;
-int	ninbuf;
-int	io;
-int	pflag;
-int	vflag	= 1;
-int	oflag;
-int	listf;
-int	listn;
-int	col;
-char	*globp;
-int	tfile	= -1;
-int	tline;
-char	*tfname;
-char	*loc1;
-char	*loc2;
-char	ibuff[BLKSIZE];
-int	iblock	= -1;
-char	obuff[BLKSIZE];
-int	oblock	= -1;
-int	ichanged;
-int	nleft;
-char	WRERR[]	= "WRITE ERROR";
+char	*nextip,	*linebp, *globp, *tfname, *loc1, *loc2, *linp	= line;
+int	vflag	= 1, tfile	= -1, iblock	= -1, oblock	= -1;
+char	WRERR[]	= "WRITE ERROR", tmpXXXXX[50] = "/tmp/eXXXXX";
 int	names[26];
-int	anymarks;
-char	*braslist[NBRA];
-char	*braelist[NBRA];
-int	nbra;
-int	subnewa;
-int	fchange;
-int	wrapp;
+char	*braslist[NBRA], *braelist[NBRA];
 unsigned nlall = 128;
-char	line[70];
-char	*linp	= line;
-char	tmpXXXXX[50] = "/tmp/eXXXXX";
-//SIG_TYP	oldhup;
-//SIG_TYP	oldquit;
-char inputbuf[GBSIZE];
-char buf[BUFSIZE];
-int bufp = 0;
-
 char	*mktemp(char *);
 long	lseek(int, long, int);
 int	close(int);
@@ -122,4 +58,3 @@ void drawline(void);
 void search_file(const char* filename, const char* searchfor);
 void process_dir(const char* dir, const char* searchfor, void (*fp)(const char*, const char*));
 void printcommand(void);
-
